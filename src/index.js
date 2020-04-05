e4d5af73d3a440f0bf235475fcdf1b89714ef267
Aldo Castro <aldocastro@Aldos-iMac.fritz.box>
2020-04-05 22:13:57 +0200

    Adding price alerts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const scrapeOvercooked2 = require('./scraper')
+const notify = require('./notifier')
 
 const scrapeIt = async () => {
   const priceGoal = 15
   const { title, url, price} = await scrapeOvercooked2()
   if (parseFloat(price) <= priceGoal) {
-    console.info('🟢 Time to buy!', {url, title, price})
+    console.info({url, title, price})
+    notify(`${title}`, `🟢 now for ${price}`, 'Time to buy it! 🤑💸💸', url, true)
   } else {
-    console.info('🔴 Still overpriced', {title, price})
+    console.info({title, price})
+    notify(`${title}`, `🔴 for ${price}`, 'Still overpriced 🐢🙄')
   }
 }
 
